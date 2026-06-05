36aa5a7c08bfe5b1a3a7ee52cee909cbaacb8c85
Alexia Zupancic <58006744+alexiazupancic@users.noreply.github.com>
2026-06-05 16:02:06 -0700

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        final_string = ""
+        for s in strs:
+            s_len = len(s) #length of following string for encoding. 
+            # Pattern -> #s_len1<string1>#s_len2<string2>
+            encoded_word = f"{s_len}#{s}"
+            final_string += encoded_word
+
+        return final_string
+
+    def decode(self, s: str) -> List[str]:
+
+        seen = []
+        for index, item in enumerate(s):
+            decoded_word = ""
+            if item.isdigit():
+                # if we find a digit ever, then we know that
+                # it is the start of a new word.
+                len_word = int(item)
+                hashtag_index = index + 1
+                # double check that we have the beginning of a word
+                if s[hashtag_index] == '#':
+                    c = 0
+                    word_starting_index = hashtag_index + 1
+                    while len(decoded_word) < len_word:
+                        decoded_word += s[word_starting_index]
+                        word_starting_index += 1
+                    seen.append(decoded_word)
+        return seen
+
